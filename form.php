2e46d10e5d68e0a99cc0000240aefbab0f34b538
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-07-16 19:30:36 +0100

    html demo form

--- form.php
+++ form.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = "us-cdbr-east-02.cleardb.com";
-$username = "b1ee0ffeb4cb3d";
-$password = "4de66d0f";
-$db = "heroku_ac5e100549d931c";
+$hostname = "us-cdbr-east-02.cleardb.com ";
+$username = "bad816b66b485e";
+$password = "7f49ef11";
+$db = "heroku_db38da5d8dd34df";
 
 $dbconnect=mysqli_connect($hostname,$username,$password,$db);
 
@@ -16,12 +16,12 @@ if(isset($_POST)) {
     $email=$_POST['email'];
     $message=$_POST['message'];
 
-    $query = "INSERT INTO `order` (`name`, `email`, `message`) VALUES ('{$name}', '{$email}', '{$message}')";
+    $query = "INSERT INTO `form_submission` (`name`, `email`, `message`) VALUES ('{$name}', '{$email}', '{$message}')";
 
     if (!mysqli_query($dbconnect, $query)) {
-        die('An error occured. Your order has not been submitted.');
+        die('An error occured.');
     } else {
-        echo "Order received, will get back to you soon.";
+        echo "Thank You! Message received";
     }
 }
 
