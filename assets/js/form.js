86294a07529b604bacaf2cd0c76b4a4cf3b4aaf4
Caspero-62 <nwaforuchechukwu2007@gmail.com>
2020-07-17 01:09:20 +0100

    html demo form

---
+++ assets/js/form.js
@@ -0,0 +1,20 @@
+// $('#form').submit(function(e){
+//     e.preventDefault();
+
+//     $.ajax({
+//         type: 'POST',
+//         url: 'form.php',
+//         data: $('#form').serialize(),
+//         success: function(data) {
+//             alert('Thank You!, Message received', data)
+//         },
+//         error: function() {
+//             alert('Oops! an error occured')
+//         },
+//         complete:function(){
+//             $('#form').each(function(){
+//                 this.reset(); 
+//             });
+//        }
+//     })
+// })
